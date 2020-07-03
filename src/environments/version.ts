b2076e42567121cd23fbba11a218df89d27dd162
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 18:55:11 -0500

    feat: p

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": false,
-    "raw": "v1.1.0-0-g6fe5d92",
-    "hash": "g6fe5d92",
+    "raw": "v1.2.0-0-gfd0fcbe",
+    "hash": "gfd0fcbe",
     "distance": 0,
-    "tag": "v1.1.0",
+    "tag": "v1.2.0",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.1.0",
+        "raw": "v1.2.0",
         "major": 1,
-        "minor": 1,
+        "minor": 2,
         "patch": 0,
         "prerelease": [],
         "build": [],
-        "version": "1.1.0"
+        "version": "1.2.0"
     },
-    "suffix": "0-g6fe5d92",
-    "semverString": "1.1.0",
-    "version": "1.1.0"
+    "suffix": "0-gfd0fcbe",
+    "semverString": "1.2.0",
+    "version": "1.2.0"
 };
 /* tslint:enable */
