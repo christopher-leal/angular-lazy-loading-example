29aaf6e193ce84dfddfe7d24042c5ec69d3f2955
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 18:17:16 -0500

    se agregaron archivos npm

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,27 +1,27 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "v0.0.1-0-gb8c251e",
-    "hash": "gb8c251e",
+    "dirty": true,
+    "raw": "v0.0.6-0-gf67374c-dirty",
+    "hash": "gf67374c",
     "distance": 0,
-    "tag": "v0.0.1",
+    "tag": "v0.0.6",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v0.0.1",
+        "raw": "v0.0.6",
         "major": 0,
         "minor": 0,
-        "patch": 1,
+        "patch": 6,
         "prerelease": [],
         "build": [],
-        "version": "0.0.1"
+        "version": "0.0.6"
     },
-    "suffix": "0-gb8c251e",
-    "semverString": "0.0.1",
-    "version": "0.0.1"
+    "suffix": "0-gf67374c-dirty",
+    "semverString": "0.0.6",
+    "version": "0.0.6"
 };
 /* tslint:enable */
