23d330802d77a60ee1d31b386386768d6084e73f
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 18:58:07 -0500

    codigo de commit BREAKING CHANGE:

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": false,
-    "raw": "v1.4.0-0-gb4f58db",
-    "hash": "gb4f58db",
+    "raw": "v1.4.1-0-g609c6db",
+    "hash": "g609c6db",
     "distance": 0,
-    "tag": "v1.4.0",
+    "tag": "v1.4.1",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.4.0",
+        "raw": "v1.4.1",
         "major": 1,
         "minor": 4,
-        "patch": 0,
+        "patch": 1,
         "prerelease": [],
         "build": [],
-        "version": "1.4.0"
+        "version": "1.4.1"
     },
-    "suffix": "0-gb4f58db",
-    "semverString": "1.4.0",
-    "version": "1.4.0"
+    "suffix": "0-g609c6db",
+    "semverString": "1.4.1",
+    "version": "1.4.1"
 };
 /* tslint:enable */
