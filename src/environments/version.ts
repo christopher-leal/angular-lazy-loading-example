4a6c46ff998e7026a14a5b2c6090a23a995e74b5
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 18:55:47 -0500

    BREAKING CHANGE: p

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": false,
-    "raw": "v1.2.0-0-gfd0fcbe",
-    "hash": "gfd0fcbe",
+    "raw": "v1.3.0-0-g630f80b",
+    "hash": "g630f80b",
     "distance": 0,
-    "tag": "v1.2.0",
+    "tag": "v1.3.0",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.2.0",
+        "raw": "v1.3.0",
         "major": 1,
-        "minor": 2,
+        "minor": 3,
         "patch": 0,
         "prerelease": [],
         "build": [],
-        "version": "1.2.0"
+        "version": "1.3.0"
     },
-    "suffix": "0-gfd0fcbe",
-    "semverString": "1.2.0",
-    "version": "1.2.0"
+    "suffix": "0-g630f80b",
+    "semverString": "1.3.0",
+    "version": "1.3.0"
 };
 /* tslint:enable */
