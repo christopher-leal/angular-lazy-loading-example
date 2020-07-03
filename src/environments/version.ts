e8d62100592e1b161c4daef783a1471fb9ca3318
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 18:39:52 -0500

    version y package

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,27 +1,27 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "v0.0.17-0-g95a16d2-dirty",
-    "hash": "g95a16d2",
+    "dirty": false,
+    "raw": "v0.0.18-0-ge2c79f1",
+    "hash": "ge2c79f1",
     "distance": 0,
-    "tag": "v0.0.17",
+    "tag": "v0.0.18",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v0.0.17",
+        "raw": "v0.0.18",
         "major": 0,
         "minor": 0,
-        "patch": 17,
+        "patch": 18,
         "prerelease": [],
         "build": [],
-        "version": "0.0.17"
+        "version": "0.0.18"
     },
-    "suffix": "0-g95a16d2-dirty",
-    "semverString": "0.0.17",
-    "version": "0.0.17"
+    "suffix": "0-ge2c79f1",
+    "semverString": "0.0.18",
+    "version": "0.0.18"
 };
 /* tslint:enable */
