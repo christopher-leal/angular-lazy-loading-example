6963e0c79fae9bc760798f4cb9c421d23c9d49eb
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 18:50:02 -0500

    BREAKING CHANGE: cambio mayor en mi codigo

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,26 +2,26 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": false,
-    "raw": "v0.0.25-0-g8900a6e",
-    "hash": "g8900a6e",
+    "raw": "v0.0.26-0-g1a32691",
+    "hash": "g1a32691",
     "distance": 0,
-    "tag": "v0.0.25",
+    "tag": "v0.0.26",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v0.0.25",
+        "raw": "v0.0.26",
         "major": 0,
         "minor": 0,
-        "patch": 25,
+        "patch": 26,
         "prerelease": [],
         "build": [],
-        "version": "0.0.25"
+        "version": "0.0.26"
     },
-    "suffix": "0-g8900a6e",
-    "semverString": "0.0.25",
-    "version": "0.0.25"
+    "suffix": "0-g1a32691",
+    "semverString": "0.0.26",
+    "version": "0.0.26"
 };
 /* tslint:enable */
