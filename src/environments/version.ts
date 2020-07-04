9ceaa9acf6d2c2320e7b2f763043ac80b41f25ab
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 19:19:36 -0500

    mensaje desde git-cz

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,27 +1,27 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "v1.4.4-0-g7478676",
-    "hash": "g7478676",
+    "dirty": true,
+    "raw": "v1.4.8-0-g0128587-dirty",
+    "hash": "g0128587",
     "distance": 0,
-    "tag": "v1.4.4",
+    "tag": "v1.4.8",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.4.4",
+        "raw": "v1.4.8",
         "major": 1,
         "minor": 4,
-        "patch": 4,
+        "patch": 8,
         "prerelease": [],
         "build": [],
-        "version": "1.4.4"
+        "version": "1.4.8"
     },
-    "suffix": "0-g7478676",
-    "semverString": "1.4.4",
-    "version": "1.4.4"
+    "suffix": "0-g0128587-dirty",
+    "semverString": "1.4.8",
+    "version": "1.4.8"
 };
 /* tslint:enable */
