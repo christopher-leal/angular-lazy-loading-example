2cbc670518825350d78ac2d8d6cab8fac8b4cc5c
christopher-leal <chris_t_o@hotmail.com>
2020-07-03 19:28:34 -0500

    fix: se cambio el script de update-version

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,27 +1,27 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "v1.4.8-1-g9ceaa9a-dirty",
-    "hash": "g9ceaa9a",
-    "distance": 1,
-    "tag": "v1.4.8",
+    "dirty": false,
+    "raw": "v2.0.0-0-gdf8b7d1",
+    "hash": "gdf8b7d1",
+    "distance": 0,
+    "tag": "v2.0.0",
     "semver": {
         "options": {
             "loose": false,
             "includePrerelease": false
         },
         "loose": false,
-        "raw": "v1.4.8",
-        "major": 1,
-        "minor": 4,
-        "patch": 8,
+        "raw": "v2.0.0",
+        "major": 2,
+        "minor": 0,
+        "patch": 0,
         "prerelease": [],
         "build": [],
-        "version": "1.4.8"
+        "version": "2.0.0"
     },
-    "suffix": "1-g9ceaa9a-dirty",
-    "semverString": "1.4.8+1.g9ceaa9a",
-    "version": "1.4.8"
+    "suffix": "0-gdf8b7d1",
+    "semverString": "2.0.0",
+    "version": "2.0.0"
 };
 /* tslint:enable */
